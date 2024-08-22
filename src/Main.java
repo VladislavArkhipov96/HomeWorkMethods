1d7c1d5e0fc0177243214869ea065f28bb727f7f
VladislavArkhipov96 <y906ee@mail.ru>
2024-08-22 16:35:32 +0300

    сделанно ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+    public static void main(String[] args) {
+        isYearleap(2024);
+        deviceClient(0,2018);
+        delyvery(30, 0);
+    }
+
+    public static void isYearleap (int year) {
+        if (year < 1589) {
+            System.out.println("Год не является високостным");
+        } else {
+            if ((year % 4 == 0 && year % 100 > 0) || year % 400 == 0) {
+                System.out.println("Год является високостным");
+            } else {
+                System.out.println("Год не является високостным");
+            }
         }
     }
+
+    //Task 2
+    public static void deviceClient (int os, int clientDeviceYear ) {
+        if (os == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (os == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (os == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (os == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+
+        }
+    }
+
+    public static void delyvery (int distance, int days) {
+        if (distance <= 20) {
+            days = 1;
+
+        } else if (distance > 20 && distance <= 60) {
+            days = 2;
+
+        } else if (distance > 60 && distance <= 100) {
+            days = 3;
+
+        } else {
+            days = -1;
+        }
+
+        System.out.println("Потребуется дней:" + days);
+    }
+
 }
